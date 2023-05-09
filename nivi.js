6acf90ca2d6b58823725f1fd75af4d04dd708fc1
thalysonlima820 <thalysonlima820@gmail.com>
2023-05-09 14:06:28 -0300

    8/6

---
+++ nivi.js
@@ -0,0 +1,242 @@
+
+
+
+function btn(){
+var agora = new Date()
+var datas = agora.getDate()
+var datacolo = Number(document.querySelector('#dat').value)
+var mes = Number(document.querySelector('#mes').value)
+var ano = Number(document.querySelector('#ano').value)
+
+
+
+    if (datacolo === datas && datacolo === 9){
+        if (mes === 5){
+            window.alert('hoje é so um teste heheheh')
+            
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 10){
+        if (mes === 5){
+            window.alert('nyvynha, saiba que voce é a unica pessoa que me faz acordar mais feliz so por saber que vc esta aqui')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 11){
+        if (mes === 5){
+            window.alert('verde')
+            document.body.classList.add ('ver')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 12){
+        if (mes === 5){
+            window.alert('como ta a especiencia em?, espero que nao tenha burlado nada em, pra valer a pena a espera de hoje me manda um  ularr que eu te mando algo especial')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 13){
+        if (mes === 5){
+            window.alert('Meu amor, nada neste mundo me faz mais feliz do que estar na sua companhia.')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 14){
+        if (mes === 5){
+            window.alert('eu vim do passado para te falar te amoo, aiii ja pensou que louco, eu to falando do passado com vc to futuro, e eu possso estar do seu lado lendo isso, queee louco')
+         } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 15){
+        if (mes === 5){
+            window.alert('nyvynha nyvynhaaaa anhaaaaa nyvynha nyvynhaaa, hoje é dia do estrese')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 16){
+        if (mes === 5){
+            window.alert('.')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 17){
+        if (mes === 5){
+            window.alert('pra valer apena hoje ja que esses dias foi zuado, manda print que hoje te pagp uma caipirinha')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 18){
+        if (mes === 5){
+            window.alert('vc ganhou um vale qualquer coisa na camaaaaa aeeee')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 19){
+        if (mes === 5){
+            window.alert('me da o cuzinho?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 20){
+        if (mes === 5){
+            window.alert('hoje é nosso aniversarioooooooo toma seu presente éeeeeeee um beijo muuchaaaass ulaaaaa ullaaaaa nyvynha')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 21){
+        if (mes === 5){
+            window.alert('hoje faremos amorrrrr me chama em gata')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 22){
+        if (mes === 5){
+            window.alert('um vale folha verde na cama emmmm hummmm eu dia de seort')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 23){
+        if (mes === 5){
+            window.alert('oii, to com salvade, vamos conversar?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 24){
+        if (mes === 5){
+            window.alert('hoje irei te deixar uma pergunta valendo um chocolate, qual minha cor favorita?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 25){
+        if (mes === 5){
+            window.alert('to sem ideia, deixa passar essa?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 26){
+        if (mes === 5){
+            window.alert('ta agora é  o seguinte dia 26 certo? vc ta lendo pelo celular deitada na cama certo? no proximo dia tenho uma grande surpresa para vc, ate amanha')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 27){
+        if (mes === 5){
+            window.alert('amanha voce terar uma surpresa muio boaaa')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 28){
+        if (mes === 5){
+            window.alert('to parecendo o raluca neeee')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 29){
+        if (mes === 5){
+            window.alert('hoje é 29, amanha ta pra ficar redondinho no 30 a grande surpresa')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 30){
+        if (mes === 5){
+            window.alert('Eu amo o seu sorriso. Amo a forma como você enxerga o mundo. Amo as suas manias. Te amo assim, meu amor: por completo. #a grande surpresa sera dia 1')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 31){
+        if (mes === 5){
+            window.alert('dica é algo que vc gosat quer muito')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 1){
+        if (mes === 6){
+            window.alert('ta vamos la, espero que nao tenha perdido nem um dia, principalmente hoje, eu nao sei quantos dias isso vai ter masssss, no fim disso tudo a ultima mensagem sera um pedido de casamento, hu,mmmmmm pode ser amanha ou depois de amanha hummm quando serar emmmmm')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 2){
+        if (mes === 6){
+            window.alert('como ta? como ta indo a esperiencia?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 3){
+        if (mes === 6){
+            window.alert('É saudade que fala? Meu amor, apesar da distância, eu sigo pensando em você e aguardando ansiosamente pelo momento em que estaremos juntos novamente. Eu te amo.')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 4){
+        if (mes === 6){
+            window.alert('eu vim do futuro, simmm do FUTO, para te falar que se vc nao der seu cu para o thalyson ele morrriii, isssssoooo, vou procvar que vim do futuro, hoje é domingo')
+            document.body.classList.add ('black')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 5){
+        if (mes === 6){
+            window.alert('vale monster')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 6){
+        if (mes === 6){
+            window.alert('me da um vinho pf?')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 7){
+        if (mes === 6){
+            window.alert('aaaaa to sem ideia entao hoje vou fazer o que vc quiser')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } else if (datacolo === datas && datacolo === 8){
+        if (mes === 6){
+            window.alert(' vale caipirinha')
+        } else{
+            window.alert('ainda nao estamos no mes em')
+        }
+       
+    } 
+    
+    
+    
+    else{
+        window.alert('tenha paciencia em')
+    }
+}
+
+
